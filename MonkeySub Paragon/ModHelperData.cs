df71eab106a6ac36d3399aed0fc30511b1eee00c
WarperSan <78861227+WarperSan@users.noreply.github.com>
2022-09-01 18:37:59 -0400

    Add files via upload

--- MonkeySub Paragon/ModHelperData.cs
+++ MonkeySub Paragon/ModHelperData.cs
@@ -9,13 +9,13 @@ namespace SubParagon
 {
     public static class ModHelperData
     {
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
         public const string Name = "Sub Paragon";
         public const string Description =
             "Adds in a custom Paragon for the Monkey Sub.";
 
         public const string RepoOwner = "WarperSan";
         public const string RepoName = "sub-paragon";
-        public const string Icon = "SubParagon-Icon.png";
+        public const string Icon = "SubParagon-Portrait.png";
     }
 }
