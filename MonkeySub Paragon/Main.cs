d44b8eec55b94b1dec55cc5b26236316ac5e5039
WarperSan <78861227+WarperSan@users.noreply.github.com>
2022-09-01 18:15:34 -0400

    Add files via upload

--- MonkeySub Paragon/Main.cs
+++ MonkeySub Paragon/Main.cs
@@ -1,9 +1,13 @@
-﻿using Assets.Scripts.Models.Towers.Behaviors;
+﻿using Assets.Scripts.Models;
+using Assets.Scripts.Models.Towers;
+using Assets.Scripts.Models.Towers.Behaviors;
 using Assets.Scripts.Models.Towers.Behaviors.Emissions;
 using Assets.Scripts.Models.Towers.Behaviors.Emissions.Behaviors;
 using Assets.Scripts.Models.Towers.Projectiles.Behaviors;
 using Assets.Scripts.Simulation.SMath;
+using Assets.Scripts.Simulation.Towers;
 using Assets.Scripts.Simulation.Towers.Behaviors;
+using Assets.Scripts.Unity;
 using Assets.Scripts.Unity.UI_New.InGame.TowerSelectionMenu;
 using BTD_Mod_Helper;
 using BTD_Mod_Helper.Extensions;
@@ -17,17 +21,38 @@ namespace SubParagon
 {
     public class Main : BloonsTD6Mod
     {
-        [HarmonyLib.HarmonyPatch(typeof(TowerSelectionMenu), "UpdateTower")]
+        [HarmonyPatch(typeof(TowerManager), "UpgradeTower")]
         internal class Tower_Initialise
         {
-            [HarmonyLib.HarmonyPrefix]
-            internal static bool Prefix()
+            [HarmonyPrefix]
+            internal static bool Prefix(int inputIndex,
+      Tower tower,
+      TowerModel def,
+      int pathIndex,
+      float upgradeCost,
+      float costMultiplier,
+      bool triggerOnUpgraded = true,
+      bool triggerOnUpgrade = true,
+      bool playUpgradeEffect = true,
+      bool isParagon = false,
+      bool leveledFromEndOfRoundXp = false)
             {
+                if(def.name == "MonkeySub-Paragon")
+                {
+                    if(tower.towerModel.tiers[0] == 5)
+                    {
+                        tower.SetTargetType(def.targetTypes[0]);
+                    }
+                    else if(tower.towerModel.tiers[2] == 5)
+                    {
+                        tower.towerModel = Game.instance.model.GetTowerFromId("MonkeySub-050");
+                    }
+                }
                 return true;
             }
         }
 
-        public override void OnTowerUpgraded(Assets.Scripts.Simulation.Towers.Tower tower, string upgradeName, Assets.Scripts.Models.Towers.TowerModel newBaseTowerModel)
+        public override void OnTowerUpgraded(Tower tower, string upgradeName, Assets.Scripts.Models.Towers.TowerModel newBaseTowerModel)
         {
             base.OnTowerUpgraded(tower, upgradeName, newBaseTowerModel);
 
@@ -35,7 +60,7 @@ namespace SubParagon
             {
                 var degree = tower.GetTowerBehavior<ParagonTower>().GetCurrentDegree();
 
-                tower.transform.position = new Vector3Boxed(
+               tower.transform.position = new Vector3Boxed(
                tower.transform.position.X,
                tower.transform.position.Y,
                tower.transform.position.Z + 12f);
@@ -44,6 +69,7 @@ namespace SubParagon
                 tower.towerModel.GetAttackModel(0).weapons[0].rate = -0.00003f * (degree - 1) + 0.03f;
                 tower.towerModel.GetWeapons()[1].projectile.GetBehavior<CreateProjectileOnExpireModel>().projectile.GetDamageModel().damage = 20000f;
                 tower.towerModel.GetWeapons()[1].rate = 5f / 7f * degree + 10f;
+                tower.towerModel.GetWeapons()[2].projectile.GetBehavior<CreateProjectileOnExpireModel>().projectile.GetDamageModel().damage = 2000f * (degree / 10 + 1);
 
                 if (degree >= 25)
                 {
